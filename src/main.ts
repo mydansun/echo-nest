344553e22662f89a572c9e58e8eac58996c61a92
mydansun <mydansun@gmail.com>
2025-06-06 17:29:09 +0100

    add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Echo Nest')
+    .setDescription('Echo Nest Document')
+    .setVersion('1.0')
+    .addBearerAuth() // 支持JWT Token输入
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
